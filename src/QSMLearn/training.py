a5d5e345ca9b40587693611ef7e8f6fdfcc12c55
Oscar Goldman <bozthemighty@gmail.com>
2022-04-05 20:37:18 +0100

    basic setup features added for packaging

---
+++ src/QSMLearn/training.py
@@ -0,0 +1,61 @@
+def main():
+    from random import random, uniform
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import tensorflow as tf
+    from tensorflow import keras
+
+    from .models import FixedStepNDI, VariableStepNDI
+    from .shapes import shapes
+
+    PARAMS = {
+        'nucleus' : 'H1',
+        'B0' : 1.5,
+        'TE' : 30e-3
+    }
+
+    REGION = (200, 250, 225)
+
+    m = FixedStepNDI(100)
+    sus = [shapes.sphere(REGION, (100, 125, 150), 50, 0.000006)]
+
+    x_train = np.array([[s.get_phase(1).numpy(), np.ones(REGION)] for s in sus])
+    y_train = np.array([s.get_tensor().numpy() for s in sus])
+
+    m.compile(
+        optimizer='adam',
+        loss=keras.losses.MeanSquaredError()
+    )
+
+    history = m.fit(
+        x = x_train,
+        y = y_train,
+        batch_size=1,
+        epochs=10
+    )
+
+def training(model_choice: str, steps: int):
+    from .models import FixedStepNDI, VariableStepNDI
+
+    MODELS = {
+        'fs' : FixedStepNDI,
+        'vs' : VariableStepNDI
+    }
+
+    m = MODELS[model_choice](steps)
+
+def trainQSM():
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description="Train a network to find optimal QSM parameters.")
+
+    parser.add_argument('model', choices=['fs', 'vs'])
+    parser.add_argument('--steps', '-s', type=int, default=10)
+
+    arguments = parser.parse_args()
+
+    training(arguments.model, arguments.steps)
+
+if __name__ == '__main__':
+    main()
